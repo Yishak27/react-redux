d025b269003fb6ebb93906016411b145ce6b47aa
Yishak27 <yisumaid3@gmail.com>
2023-06-01 08:02:01 +0300

    using react toolkit for redux file

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,37 @@
 import { createStore } from 'redux'
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 //creating the reducer
-const counterReducer = (state = { counter: 0, counterTouch: false }, action) => {
-    if (action.type === 'increment') {
-        return { counter: state.counter + action.value, counterTouch: state.counterTouch }
+const initialState = { counter: 0, counterTouch: false }
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state, action) {
+            state.counter += action.value
+        },
+        decrement(state) {
+            state.counter--;
+        },
+        counterTouch(state) {
+            state.counterTouch = !state.counterTouch;
+        }
     }
-    if (action.type === 'decrement') {
-        return { counter: state.counter - 1, counterTouch: state.counterTouch }
-    }
-    if (action.type === "touch") {
-        return { counterTouch: !state.counterTouch, counter: state.counter }
-    }
-    return state;
-}
-//creating the store
-const store = createStore(counterReducer);
+})
+const store = configureStore({ reducer: counterSlice.reducer })
+// const counterReducer = (state =initialState , action) => {
+//     if (action.type === 'increment') {
+//         return { counter: state.counter + action.value, counterTouch: state.counterTouch }
+//     }
+//     if (action.type === 'decrement') {
+//         return { counter: state.counter - 1, counterTouch: state.counterTouch }
+//     }
+//     if (action.type === "touch") {
+//         return { counterTouch: !state.counterTouch, counter: state.counter }
+//     }
+//     return state;
+// }
+// //creating the store
+// const store = createStore(counterReducer);
 
 // making an action
 
