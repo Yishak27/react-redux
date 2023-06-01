ad0c64b2921dc8aff8e8cb81f6ff67f135f5d5d2
Yishak27 <yisumaid3@gmail.com>
2023-06-01 07:41:39 +0300

    able to use redux with more than one states

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,22 +1,24 @@
 import classes from './Counter.module.css';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Counter = () => {
-  const dispatch =useDispatch();
+  const dispatch = useDispatch();
   const counters = useSelector(state => state.counter);
+  const countTouch = useSelector(state => state.counterTouch);
 
   const toggleCounterHandler = () => {
+    dispatch({ type: 'touch' })
   };
-  const incrementHandler =()=>{
-    dispatch({type:'increment'})
+  const incrementHandler = () => {
+    dispatch({ type: 'increment', value: 3 })
   }
-  const decrementHandler =()=>{
-    dispatch({type:'decrement'})
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' })
   }
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counters}</div>
+      {countTouch && <div className={classes.value}>{counters}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import { createStore } from 'redux'
 //creating the reducer
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, counterTouch: false }, action) => {
     if (action.type === 'increment') {
-        return { counter: state.counter + 1 }
+        return { counter: state.counter + action.value, counterTouch: state.counterTouch }
     }
     if (action.type === 'decrement') {
-        return { counter: state.counter - 1 }
+        return { counter: state.counter - 1, counterTouch: state.counterTouch }
+    }
+    if (action.type === "touch") {
+        return { counterTouch: !state.counterTouch, counter: state.counter }
     }
     return state;
 }
